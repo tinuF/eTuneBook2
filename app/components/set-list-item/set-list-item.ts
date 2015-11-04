34c4b12ac5dd7afee14bf2cb96f9a4fb0623eefc
Martin Fleischmann <martfle@bluewin.ch>
2015-11-04 23:10:29 +0100

    sort TuneSetPosition, style tune actions

--- app/components/set-list-item/set-list-item.ts
+++ app/components/set-list-item/set-list-item.ts
@@ -4,6 +4,7 @@ import {Router, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {TuneBookService} from '../../services/tunebook-service';
 import {TuneSet} from '../../business/model/tuneset';
+import {TuneSetPosition} from '../../business/model/tunesetposition';
 import {EliminateThe} from '../../pipes/eliminate-the';
 import {FromNow} from '../../pipes/from-now';
 import {SampleDotsUI} from '../../components/sample-dots/sample-dots';
@@ -28,13 +29,20 @@ export class SetListItemUI {
   onInit() {
     //needs jQuery UI
     //jQuery(this.elementRef.nativeElement).draggable({containment:'#draggable-parent'});
+    this.sortSetPosition();
   }
   
   justPlayedTheSet() {
 		var now = new Date();
 		this.tuneBookService.addTuneSetPlayDate(this.set, now);
 		this.tuneBookService.storeTuneBookAbc();
-	};
+	}
+  
+  sortSetPosition(){
+    this.set.tuneSetPositions.sort(function(a:TuneSetPosition, b:TuneSetPosition) {
+        return a.position - b.position
+    })
+  }
 }
 
 
